4ba271670a0c2258637f495a671af276d24cc973
Pavel Milanov <milanov71@icloud.com>
2024-05-24 16:43:00 +0300

    добавлен прогрессбар

--- git.go
+++ git.go
@@ -108,10 +108,10 @@ func gitMerge(localBranch, deployBranch string) {
 // Производит git fetch, git merge, docker build, docker up исходя из изменений в коммитах.
 // Собирает и запускает сервисы в указанном файле docker-compose, где обновились файлы.
 func deploy(config Environments) {
-	go STAGE1.Add(1)
+	STAGE1.Add(1)
 	gitFetch()
 	changes := gitDiff(config.Local, config.Remote)
-	go STAGE2.Add(1)
+	STAGE2.Add(1)
 	services := parseDockerCompose(config.Docker)
 	updateServices := analuzeChanges(services, changes)
 	if len(updateServices) == 0 {
@@ -123,11 +123,11 @@ func deploy(config Environments) {
 		text := fmt.Sprintf(" - %s ", service.Name)
 		fmt.Println(text)
 	}
-	go STAGE3.Add(1)
+	STAGE3.Add(1)
 	branch := createDeployBranch(config.Remote)
 	gitMerge(config.Local, branch)
 	deleteDeployBranch(branch)
-	go STAGE4.Add(1)
+	STAGE4.Add(1)
 	buildServices := buildDockerCompose(updateServices, config.Docker)
 	upDockerCompose(buildServices, config.Docker)
 	fmt.Println("Обновление завершено")
