5fd28cd6251c8a1a80e4abca704558f171a2a18f
Pavel Milanov <milanov71@icloud.com>
2024-08-06 09:43:18 +0300

    устранен баг в скрипте инсталяции

--- install.sh
+++ install.sh
@@ -6,4 +6,3 @@ wget https://github.com/PavelMilanov/opencd/releases/download/v0.2.1/opencd
 chmod +x ./opencd
 mv opencd /usr/bin/
 sh -c "echo export PATH=$PATH:/usr/bin/opencd >> /etc/profile"
-source /etc/profile
